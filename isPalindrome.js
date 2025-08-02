5666a966a6459d04e4c1ab7cb891010c5e486558
lyklecharova <lklecharova9@gmail.com>
2025-08-02 12:01:21 +0300

    Initial commit

--- isPalindrome.js
+++ isPalindrome.js
@@ -2,4 +2,5 @@ function isPalindrome(str) {
   const clean = str.toLowerCase().replace(/[^a-z0-9]/g, "");
   return clean === clean.split("").reverse().join("");
 }
+
 module.exports = isPalindrome;
--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -1,10 +1,13 @@
 const isPalindrome = require("./isPalindrome");
+
 test('"racecar" should be a palindrome', () => {
   expect(isPalindrome("racecar")).toBe(true);
 });
+
 test('"hello" should not be a palindrome', () => {
   expect(isPalindrome("hello")).toBe(false);
 });
+
 test('"A man, a plan, a canal: Panama" should be a palindrome', () => {
   expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
 });
