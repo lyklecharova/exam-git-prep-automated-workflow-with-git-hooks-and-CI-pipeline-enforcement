462fbd6d55006ba9cf15f994aa8421cf0726640d
lyklecharova <lklecharova9@gmail.com>
2025-08-02 11:54:25 +0300

    Fix Prettier issues

--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -1,13 +1,10 @@
-const isPalindrome = require('./isPalindrome');
-
+const isPalindrome = require("./isPalindrome");
 test('"racecar" should be a palindrome', () => {
-  expect(isPalindrome('racecar')).toBe(true);
+  expect(isPalindrome("racecar")).toBe(true);
 });
-
 test('"hello" should not be a palindrome', () => {
-  expect(isPalindrome('hello')).toBe(false);
+  expect(isPalindrome("hello")).toBe(false);
 });
-
 test('"A man, a plan, a canal: Panama" should be a palindrome', () => {
-  expect(isPalindrome('A man, a plan, a canal: Panama')).toBe(true);
+  expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
 });
